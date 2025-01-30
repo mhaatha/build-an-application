abb8a09b9a6cf5cc03ce18c95fc7505556e79ca5
mhaatha <mhafidza528@gmail.com>
2025-01-30 13:50:06 +0800

    Change the integration test

--- http-server/server_integration_test.go
+++ http-server/server_integration_test.go
@@ -13,7 +13,7 @@ func TestRecordingWinsAndRetrievingThem(t *testing.T) {
 	}
 	store := NewPostgresPlayerStore(db)
 	server := PlayerServer{store}
-	player := "Player"
+	player := "NewPlayer"
 
 	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
 	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
