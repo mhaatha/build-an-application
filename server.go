a1cb96a46f215a65c8773d9deaa2fe35e832dfb6
mhaatha <mhafidza528@gmail.com>
2025-01-31 17:37:53 +0800

    Refactor by move the application/json as a contstant type

--- server.go
+++ server.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 )
 
+const jsonContentType = "application/json"
+
 type Player struct {
 	Name string
 	Wins int
@@ -40,7 +42,7 @@ func NewPlayerServer(store PlayerStore) *PlayerServer {
 }
 
 func (p *PlayerServer) leagueHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("content-type", "application/json")
+	w.Header().Set("content-type", jsonContentType)
 	json.NewEncoder(w).Encode(p.store.GetLeague())
 }
 
