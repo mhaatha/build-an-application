00c6039f45cc04a0cc0795e722b8e5210092632d
mhaatha <mhafidza528@gmail.com>
2025-01-31 17:26:32 +0800

    Make InMemoryPlayerStore implements PlayerStore interface

--- in_memory_player_store.go
+++ in_memory_player_store.go
@@ -30,3 +30,7 @@ func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
 	defer i.lock.RUnlock()
 	return i.store[name]
 }
+
+func (i *InMemoryPlayerStore) GetLeague() []Player {
+	return nil
+}
