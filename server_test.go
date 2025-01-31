799fc7a6cd3f35ff0d8e7d8c92868c06a059eaa2
mhaatha <mhafidza528@gmail.com>
2025-01-31 17:40:01 +0800

    Create a content type assertion in the test

--- server_test.go
+++ server_test.go
@@ -125,11 +125,15 @@ func TestLeague(t *testing.T) {
 		got := getLeagueFromResponse(t, response.Body)
 		assertStatus(t, response.Code, http.StatusOK)
 		assertLeague(t, got, wantedLeague)
-
-		if response.Result().Header.Get("content-type") != "application/json" {
-			t.Errorf("response did not have content-type of application/json, got %v", response.Result().Header)
-		}
+		assertContentType(t, response, jsonContentType)
 	})
+}
+
+func assertContentType(t testing.TB, response *httptest.ResponseRecorder, want string) {
+	t.Helper()
+	if response.Result().Header.Get("content-type") != want {
+		t.Errorf("response did not have content-type of %s, got %v", want, response.Result().Header)
+	}
 }
 
 func getLeagueFromResponse(t testing.TB, body io.Reader) (league []Player) {
