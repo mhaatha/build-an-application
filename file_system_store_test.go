dbb7b4a91548e4c85bd32e3fcab170d097d83932
mhaatha <mhafidza528@gmail.com>
2025-02-01 13:08:12 +0800

    Create a test for sorted league endpoint

--- file_system_store_test.go
+++ file_system_store_test.go
@@ -103,6 +103,26 @@ func TestFileSystemStore(t *testing.T) {
 
 		assertNoError(t, err)
 	})
+
+	t.Run("league sorted", func(t *testing.T) {
+		database, cleanDatabase := createTempFile(t, `[
+			{"Name": "Cleo", "Wins": 10},
+			{"Name": "Chris", "Wins": 33}]`)
+		defer cleanDatabase()
+
+		store, err := NewFileSystemPlayerStore(database)
+		assertNoError(t, err)
+
+		got := store.GetLeague()
+		want := League{
+			{"Chris", 33},
+			{"Cleo", 10},
+		}
+		assertLeague(t, got, want)
+
+		got = store.GetLeague()
+		assertLeague(t, got, want)
+	})
 }
 
 func assertNoError(t testing.TB, err error) {
