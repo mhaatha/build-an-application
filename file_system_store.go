74962338483a57d24c222e32fb6687a5fbfd2631
mhaatha <mhafidza528@gmail.com>
2025-02-01 10:23:23 +0800

    Make the test pass by implements io.ReadSeeker and use Seek to reset the read from the beginning of a file

--- file_system_store.go
+++ file_system_store.go
@@ -5,10 +5,12 @@ import (
 )
 
 type FileSystemPlayerStore struct {
-	database io.Reader
+	database io.ReadSeeker
 }
 
 func (f *FileSystemPlayerStore) GetLeague() []Player {
+	// 0 (no movements) SeekStart (Start from the beginning of a file)
+	f.database.Seek(0, io.SeekStart)
 	league, _ := NewLeague(f.database)
 	return league
 }
