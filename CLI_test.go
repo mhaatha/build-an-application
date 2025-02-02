134464383beed2d119a3a792516e6c34a5bf9765
mhaatha <mhafidza528@gmail.com>
2025-02-02 20:02:16 +0800

    Fix typo in the test for Cleo

--- CLI_test.go
+++ CLI_test.go
@@ -17,7 +17,7 @@ func TestCLI(t *testing.T) {
 	})
 
 	t.Run("record cleo win from user input", func(t *testing.T) {
-		in := strings.NewReader("Chris wins\n")
+		in := strings.NewReader("Cleo wins\n")
 		playerStore := &StubPlayerStore{}
 
 		cli := &CLI{playerStore, in}
